f0943a515376efa2760338977144b43540f3950e
JustasSub <justas.subatavicius@telesoftas.com>
2021-09-16 18:43:08 +0300

    implement caching on itunes artist data loader

--- src/main/java/lt/justas/demo/app/config/ItunesDataLoader.java
+++ src/main/java/lt/justas/demo/app/config/ItunesDataLoader.java
@@ -6,11 +6,14 @@ import lt.justas.demo.integration.itunes.parse.ItunesArtistTopAlbumsParser;
 import lt.justas.demo.integration.itunes.parse.ItunesArtistsDataParser;
 import lt.justas.demo.model.dto.AlbumDTO;
 import lt.justas.demo.model.dto.ArtistDTO;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames = {"itunes-artists"})
 public class ItunesDataLoader {
     private final ItunesArtistsFetcher itunesArtistsFetcher;
     private final ItunesArtistsDataParser itunesArtistsDataParser;
@@ -27,11 +30,13 @@ public class ItunesDataLoader {
         this.itunesArtistTopAlbumsParser = itunesArtistTopAlbumsParser;
     }
 
+    @Cacheable(key = "#artistName")
     public List<ArtistDTO> loadArtistsByName(String artistName) {
         var data = itunesArtistsFetcher.fetchData(artistName);
         return itunesArtistsDataParser.parseData(data);
     }
 
+    @Cacheable(key = "#artistId")
     public List<AlbumDTO> loadArtistTopAlbums(Long artistId) {
         var data = itunesArtistTopAlbumsFetcher.fetchData(artistId);
         return itunesArtistTopAlbumsParser.parseData(data);
